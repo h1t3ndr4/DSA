6e56b17970e5388792b35ed167f38729350494dc
h1t3ndr4 <hitumali10@gmail.com>
2022-03-24 12:22:28 +0530

    7

---
+++ All DSA Questions/Cube Of A Number.js
@@ -0,0 +1,25 @@
+// Description
+
+// Given a number N, find its cube.
+
+// Input
+// The first and the only line of the input contains the number N.
+
+// Constraints
+
+// 1 <= N <= 30
+
+// Output
+// Print the cube of a number.
+
+// Sample Input 1
+
+// 2
+// Sample Output 1
+
+// 8
+
+function cubeOfNumber(N) {
+  var result = N ** 3;
+  console.log(result);
+}
