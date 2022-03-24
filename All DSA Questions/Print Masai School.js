fa81295210038f2fdcdbb9dd78d871306d2588a3
h1t3ndr4 <hitumali10@gmail.com>
2022-03-24 12:11:29 +0530

    1

---
+++ All DSA Questions/Print Masai School.js
@@ -0,0 +1,20 @@
+// Description
+
+// You need to print "Masai School" on a line, and print "A Transformation in Education" on a new line.
+
+// Input
+// No input
+
+// Output
+// Print "Masai School" on a line, and Print "A Transformation in Education" on a new line.
+
+// Sample Input 1
+
+// No Input
+// Sample Output 1
+
+// Masai School
+// A Transformation in Education
+
+console.log("Masai School");
+console.log("A Transformation in Education");
