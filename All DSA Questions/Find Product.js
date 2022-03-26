98b3aee0ce7bdd50663eab51e0582f2f11c06131
h1t3ndr4 <hitumali10@gmail.com>
2022-03-26 09:18:45 +0530

    10

---
+++ All DSA Questions/Find Product.js
@@ -0,0 +1,30 @@
+// Description
+
+// You are given five numbers stored in variables with the following names
+
+// one, two, three, four, five
+
+// Find the product of the five values, and print it
+
+// Input
+// The first and the only line of the input contains five values, stored in the variablesone, two, three, four, five
+
+// Output
+// Print the product of the five values stored in the variablesone, two, three, four, five
+
+// Sample Input 1
+
+// 1 2 3 4 5
+// Sample Output 1
+
+// 120
+// Hint
+
+// In the sample test case, the values stored in the variables areone = 1, two = 2, three = 3, four = 4, five= 5
+
+// The sum of the values is1 * 2 * 3 * 4 * 5 = 120, which is the required sum
+
+function findProduct(one, two, three, four, five) {
+  var result = one * two * three * four * five;
+  console.log(result);
+}
