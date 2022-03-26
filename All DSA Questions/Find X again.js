f2ec1edb7722bf615b9b46fef607e2e08b63a673
h1t3ndr4 <hitumali10@gmail.com>
2022-03-26 09:24:03 +0530

    14

---
+++ All DSA Questions/Find X again.js
@@ -0,0 +1,30 @@
+// Description
+
+// Given a value of X, find the final value of X, if X is multiplied by 7 twice.
+
+// Input
+// Input:
+
+// Input will contain one number X
+
+// Output
+// Output:
+
+// Output should be the number X after performing above multiply operations
+
+// Sample Input 1
+
+// 6
+// Sample Output 1
+
+// 294
+// Hint
+
+// Explanation
+
+// The value of X = 6, so after multiplying by 7 twice, it becomes 6 x 7 x 7, it becomes 294
+
+function findX(x) {
+  var result = x * 7 * 7;
+  console.log(result);
+}
