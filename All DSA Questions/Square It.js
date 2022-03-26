21523d1a8cae3e8208a35d019e30f1180794df66
h1t3ndr4 <hitumali10@gmail.com>
2022-03-26 09:17:44 +0530

    9

---
+++ All DSA Questions/Square It.js
@@ -0,0 +1,27 @@
+// Description
+
+// You are given an integer N, print its  [square value](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/pow) (number raised to exponent 2)
+
+// Input
+// Input Format
+
+// First and the only line contains one integer N
+
+// Constraints
+
+// N<100
+
+// Output
+// Output an integer which is square of N
+
+// Sample Input 1
+
+// 7
+// Sample Output 1
+
+// 49
+
+function squareIt(N) {
+  var result = N ** 2;
+  console.log(result);
+}
