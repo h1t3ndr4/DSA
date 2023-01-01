b9d332da2ba09d3788cfe211234db3bfc63908a6
h1t3ndr4 <hitumali10@gmail.com>
2023-01-01 19:33:21 +0530

    practice with compare two numbers

---
+++ All DSA Questions/Compare two Numbers.js
@@ -0,0 +1,13 @@
+// You are given two numbers stored in two variables, with the name, num1 and num2
+// Print the result of the following operations
+// 1. num1 > num2 -> this operation prints true, if num1 is greater than num2, otherwise it prints false
+// 2. num1 < num2 -> this operations prints true, if num1 is smaller than num2, otherwise it prints false
+// 3. num1 == num2 -> this operation print true, if num1 is equal to num2, otherwise it prints false
+// You have to print the result of three operations in the order as shown above
+
+// Write your code here
+let num1 = 3;
+let num2 = 5;
+console.log(num1 > num2);
+console.log(num1 < num2);
+console.log(num1 == num2);
