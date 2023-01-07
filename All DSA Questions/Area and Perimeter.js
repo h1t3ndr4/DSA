ccb2d5226854481eb51a239462e9ed9d79f1652d
h1t3ndr4 <hitumali10@gmail.com>
2023-01-07 12:34:29 +0530

    Create Area and Perimeter.js

---
+++ All DSA Questions/Area and Perimeter.js
@@ -0,0 +1,15 @@
+// You are given the sides of the rectangle 1: L1 and B1 and sides of the rectangle 2 : L2 and B2.
+// You need print if area of rectangle 1 is greater than rectangle 2.
+// You need print if perimeter of rectangle 1 is greater than rectangle 2.
+
+// Write your code here
+let L1 = 1;
+let B1 = 2;
+let L2 = 3;
+let B2 = 4;
+let area1 = L1 * B1;
+let area2 = L2 * B2;
+let perimeter1 = 2 * (L1 + B1);
+let perimeter2 = 2 * (L2 + B2);
+console.log(area1 > area2);
+console.log(perimeter1 > perimeter2);
