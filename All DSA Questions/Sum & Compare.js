76a2ede5045ed5ef963517854c3414969b9df513
h1t3ndr4 <hitumali10@gmail.com>
2023-01-07 12:35:03 +0530

    Create Sum & Compare.js

---
+++ All DSA Questions/Sum & Compare.js
@@ -0,0 +1,16 @@
+// You are given five numbers, stored in the variables with the following name
+//  one, two, three, four, five
+// You have to declare another variable sum1, such that sum1 = one + two + three
+// Also, you have to declare another variable sum2, such that sum2 = four + five
+// Finally, you have to print the output of the following operation
+//  sum1 > sum2 (This operation prints true, if sum1 is greater than sum2, else it prints false)
+
+// Write your code here
+let one = 1;
+let two = 2;
+let three = 3;
+let four = 4;
+let five = 5;
+let sum1 = one + two + three;
+let sum2 = four + five;
+console.log(sum1 > sum2);
