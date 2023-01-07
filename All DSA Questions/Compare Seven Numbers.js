ad9c0ae0160a14d087f56ba9f80f07f31dc31177
h1t3ndr4 <hitumali10@gmail.com>
2023-01-07 12:34:51 +0530

    Create Compare Seven Numbers.js

---
+++ All DSA Questions/Compare Seven Numbers.js
@@ -0,0 +1,19 @@
+// You are given seven numbers, stored in the variable with the following names
+// a,b,c,d,e,f,g
+// You have to declare two variables,sum1 and sum2, such that sum1 = (a + b) * c and sum2 = (d + e + f + g)
+// Print the output of the following operation
+
+// sum1 > sum2
+
+// Write your code here
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+let e = 5;
+let f = 6;
+let g = 7;
+let sum1 = (a + b) * c;
+let sum2 = d + e + f + g;
+console.log(sum1 > sum2);
+
