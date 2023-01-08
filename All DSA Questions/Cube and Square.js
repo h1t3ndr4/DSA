d4f5be5f774eae3124641aa6f0aad9a36e68dfb9
h1t3ndr4 <hitumali10@gmail.com>
2023-01-08 18:36:12 +0530

    Create Cube and Square.js

---
+++ All DSA Questions/Cube and Square.js
@@ -0,0 +1,8 @@
+// You are given two numbers n and m, you need to check if cube of n is greater than square of m.
+
+//write your code here
+
+let n = 2;
+let m = 3;
+
+n ** 3 > m ** 2 ? console.log("Yes") : console.log("No");
