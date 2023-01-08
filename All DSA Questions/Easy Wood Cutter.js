9c53e6e4a29794bdd6e11fb5a14c9b58896ff519
h1t3ndr4 <hitumali10@gmail.com>
2023-01-08 18:36:41 +0530

    Create Easy Wood Cutter.js

---
+++ All DSA Questions/Easy Wood Cutter.js
@@ -0,0 +1,10 @@
+// You are given a number, stored in a variable with the nameN. Check if the the number is divisible by 3 or not
+// If its possible, print "Yes"
+// Else print "No"
+// Hint : A number can be divided into 3 parts, if the number is completely divisible by 3, that is, the answer of the operation number % 3 is zero
+
+//write your code here
+
+let N = 3;
+
+N % 3 == 0 ? console.log("Yes") : console.log("No");
