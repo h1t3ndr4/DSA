008c9dcd3d0144d1c52dd0295d1a6c8e634d6884
h1t3ndr4 <hitumali10@gmail.com>
2023-01-08 18:36:30 +0530

    Create Divisible by 4.js

---
+++ All DSA Questions/Divisible by 4.js
@@ -0,0 +1,13 @@
+// You are given a number, stored in the variable with the name N
+// PrintYes, if the number is divisible by 4, else print No
+// Note : A number is divisible by 4, if the result of the following expression number % 4 == 0
+
+let N = 4;
+
+if (N % 4 == 0) {
+    console.log("Yes");
+}
+else {
+    console.log("No");
+}
+
