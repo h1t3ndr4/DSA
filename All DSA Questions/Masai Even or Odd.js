05bda5494eed1a41f4c6b8bdb6d801af26efefe9
h1t3ndr4 <hitumali10@gmail.com>
2023-01-14 17:07:03 +0530

    Create Masai Even or Odd.js

---
+++ All DSA Questions/Masai Even or Odd.js
@@ -0,0 +1,9 @@
+// You are given a number, stored in the variable with the name N
+// If the number is even, print Even, else print Odd
+// If the number is divisible by 2, the number is considered Even, else the number is considered Odd
+
+let N = 10
+
+N % 2 == 0 ? console.log("Even") : console.log("Odd")
+
+
