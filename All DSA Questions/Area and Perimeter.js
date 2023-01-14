70d240b69a3193540ff2f7dbdbfa1e08d9e440c7
h1t3ndr4 <hitumali10@gmail.com>
2023-01-14 17:06:27 +0530

    Update Area and Perimeter.js

--- All DSA Questions/Area and Perimeter.js
+++ All DSA Questions/Area and Perimeter.js
@@ -3,13 +3,15 @@
 // You need print if perimeter of rectangle 1 is greater than rectangle 2.
 
 // Write your code here
-let L1 = 1;
-let B1 = 2;
-let L2 = 3;
-let B2 = 4;
-let area1 = L1 * B1;
-let area2 = L2 * B2;
-let perimeter1 = 2 * (L1 + B1);
-let perimeter2 = 2 * (L2 + B2);
+let L1 = 1,
+    B1 = 2,
+    L2 = 3,
+    B2 = 4,
+    area1 = L1 * B1,
+    area2 = L2 * B2,
+    perimeter1 = 2 * (L1 + B1),
+    perimeter2 = 2 * (L2 + B2)
+
 console.log(area1 > area2);
 console.log(perimeter1 > perimeter2);
+
