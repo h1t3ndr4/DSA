3209cb5cef4d20e9bb8ad74c8203e8140164f8ca
h1t3ndr4 <hitumali10@gmail.com>
2023-01-15 21:02:41 +0530

    Create Enough Fuel Consumption.js

---
+++ All DSA Questions/Enough Fuel Consumption.js
@@ -0,0 +1,13 @@
+// You are given two numbers, stored in the variable with the following names
+// fuel, distance
+// Find the value of required, such that
+// required = fuel*distance 
+// If the value ofcrequiredcis greater than 50, printcEnough, else print Go On
+
+let fuel = 10,
+    distance = 5,
+    required = fuel * distance;
+
+required > 50 ? console.log("Enough") : console.log("Go On");
+
+
