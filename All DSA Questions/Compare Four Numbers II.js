a18d5c3069fb685d7d3122eb8fa26cd33c8e365e
h1t3ndr4 <hitumali10@gmail.com>
2023-01-21 09:21:26 +0530

    Create Compare Four Numbers II.js

---
+++ All DSA Questions/Compare Four Numbers II.js
@@ -0,0 +1,12 @@
+// You are given four numbers stored in variables with the following names
+// one, two, three, four
+// If the value of any one of the following expression is true, printYes, else print No
+// 1. one > two
+// 2. three > four
+
+let one = 10,
+    two = 20,
+    three = 30,
+    four = 40;
+
+one > two || three > four ? console.log("Yes") : console.log("No");
