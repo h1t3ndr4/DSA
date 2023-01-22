3daf09608b4189f039143abb02ca5266d39632e6
h1t3ndr4 <hitumali10@gmail.com>
2023-01-22 11:24:40 +0530

    Create Odd, Even and Both.js

---
+++ All DSA Questions/Odd, Even and Both.js
@@ -0,0 +1,12 @@
+// You are given two numbers, stored in the variable with the following names
+// one, two
+// If both the values are even, print Even
+// Else if both the values are odd, print Odd
+// else print Even-Odd
+
+let one = 1,
+    two = 2;
+
+one % 2 == 0 && two % 2 == 0 ? console.log('Even') : one % 2 != 0 && two % 2 != 0 ? console.log('Odd') : console.log('Even-Odd');
+
+
