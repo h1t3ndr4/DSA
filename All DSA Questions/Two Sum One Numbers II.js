cd97f4cdf3e0303be542bb5ad13d2b7f3626f9df
h1t3ndr4 <hitumali10@gmail.com>
2023-01-22 11:24:22 +0530

    Create Two Sum One Numbers II.js

---
+++ All DSA Questions/Two Sum One Numbers II.js
@@ -0,0 +1,17 @@
+// You are given five numbers, stored in the variables with the following names
+// one, two, three, four, five
+// Find the value of sum1 and sum2, such that
+// sum1 = one + two 
+// sum2 = three + four
+// If the value of sum1 or sum2 is greater than the value stored in variable five, print Yes, else print No
+
+let one = 1,
+    two = 2,
+    three = 3,
+    four = 4,
+    five = 5,
+
+    sum1 = one + two,
+    sum2 = three + four;
+
+sum1 > five || sum2 > five ? console.log('Yes') : console.log('No');
