941d7d4270ab1c69e5322eccd76ec6ade3067031
h1t3ndr4 <hitumali10@gmail.com>
2023-01-22 11:24:52 +0530

    Create Logical Evens.js

---
+++ All DSA Questions/Logical Evens.js
@@ -0,0 +1,10 @@
+// You are given two numbers, stored in the variable with the following names
+// one, two
+// If both the values are Even, print Both, else print No
+
+let one = 1,
+    two = 2;
+
+one % 2 == 0 && two % 2 == 0 ? console.log('Both') : console.log('No');
+
+
