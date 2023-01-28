09ac258050f613df512f62381b9825ca540900c7
h1t3ndr4 <hitumali10@gmail.com>
2023-01-28 17:45:21 +0530

    Create Print Masai School II.js

---
+++ All DSA Questions/Print Masai School II.js
@@ -0,0 +1,16 @@
+// You are given a number stored in a variable with the name num
+// You have to print Masai School, as many times as the number stored in the variable num
+// Every time, print Masai School on a new line
+
+let num = 5;
+
+for (let i = 0; i < num; i++) {
+    console.log('Masai School');
+}
+
+//or
+
+for (let i = 1; i <= num; i++) {
+    console.log('Masai School');
+}
+
