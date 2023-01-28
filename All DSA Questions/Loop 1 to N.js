191ad150bd4908985070cc636bcc38b2c575d9dc
h1t3ndr4 <hitumali10@gmail.com>
2023-01-28 17:44:57 +0530

    Create Loop 1 to N.js

---
+++ All DSA Questions/Loop 1 to N.js
@@ -0,0 +1,18 @@
+// You are given a number stored in a variable with the name num
+// You have to print all the numbers from 1 to num, including num as well.
+// Print each number on a new line
+
+let num = 5;
+
+for (let i = 1; i <= num; i++) {
+    console.log(i);
+}
+
+//or
+
+for (let i = 0; i < num; i++) {
+    console.log(i + 1);
+}
+
+
+
