0177ce40d0eafcf08bbfb02210f90f8007992dee
h1t3ndr4 <hitumali10@gmail.com>
2023-02-12 18:53:40 +0530

    Create Print Array.js

---
+++ All DSA Questions/Print Array.js
@@ -0,0 +1,38 @@
+// You are given an array, whose size is stored in a variable of size n
+// The array is stored in a variable with the name, arr
+// You have to traverse the array, and print each element on a new line
+// For example, the value stored in N = 5, and the array is arr = [1 2 3 4 5]
+// Therefore, the required output will be
+// 1
+// 2
+// 3
+// 4
+// 5
+
+let N = 5,
+    arr = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < N; i++) {
+    console.log(arr[i]);
+}
+
+//or
+
+//travese the array
+
+arr.forEach((element) => {
+    console.log(element);
+});
+
+//or
+
+arr.map((element) => {
+    console.log(element);
+})
+
+//or
+
+
+
+
+
