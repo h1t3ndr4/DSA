fb95ce3cf1fc3a7be2c0152bc86e342e7b637ba1
h1t3ndr4 <hitumali10@gmail.com>
2023-02-18 23:54:35 +0530

    Create Odd Array.js

---
+++ All DSA Questions/Odd Array.js
@@ -0,0 +1,21 @@
+//print the odd values from an array on a new line
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 !== 0) {
+        console.log(arr[i]);
+    }
+}
+
+//or
+
+let Arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < Arr.length; i++) {
+    if (Arr[i] % 2 === 0) {
+        continue;
+    }
+    console.log(Arr[i]);
+}
+
